9a727aaaffb938d75f2c1d511f443513a40fe547
Ahmed Gedi <ahmed.gedi1@gmail.com>
2020-07-07 23:43:36 -0700

    preparing gui before implementing scan scripts

--- main.py
+++ main.py
@@ -3,17 +3,28 @@ import tkinter as tk
 window = tk.Tk()
 window.title('Wifi Device scanner')
 
+def start_scan():
+    return print('Scanning.....')
+
+
 header = tk.Label(text="Devices on your network")
-# creating the rows
-for i in range(1,6):
-    window.columnconfigure(0, weight=1, minsize=50)
+scan_btn = tk.Button(master=window, text="Scan your network", command=start_scan)
 
-    frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=2)
-    frame.grid(row=i, column=0, padx=3, pady=3, sticky="ew")
+devices_found = ['No devices found']
 
-    lbl = tk.Label(master=frame, text=f"Device {i}", bg="white")
-    lbl.pack(fill=tk.X)
 
-header.grid(row=0, sticky="ew")
+for i in devices_found:
+    for j in range(1, len(devices_found) + 1):
+        window.columnconfigure(0, weight=1, minsize=50)
+
+        frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=2)
+        frame.grid(row=j, column=0, padx=3, pady=3, sticky="ew")
+
+        lbl = tk.Label(master=frame, text=i, bg="white")
+        lbl.pack(fill=tk.X)
+
+header.grid(row=1, sticky="ew")
+scan_btn.grid(row=0, column=0, sticky="ew", pady=2)
+
 
 window.mainloop()
