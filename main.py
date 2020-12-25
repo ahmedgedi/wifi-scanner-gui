57d3e95e9f900d0ffe7694caafb5a6bc9581e657
Ahmed Gedi <ahmed.gedi1@gmail.com>
2020-12-25 03:27:30 -0800

    interactive UI done, semi functional scan working. needs to be finetuned

--- main.py
+++ main.py
@@ -1,30 +1,63 @@
 import tkinter as tk
+import tkinter.messagebox
+import sys, socket
 
 window = tk.Tk()
 window.title('Wifi Device scanner')
 
-def start_scan():
-    return print('Scanning.....')
+header = tk.Label(text="Open ports on your network")
+router_ip= tk.Entry(master=window, width=50)
+
+devices_found = []
 
 
-header = tk.Label(text="Devices on your network")
-scan_btn = tk.Button(master=window, text="Scan your network", command=start_scan)
+def list_devices(devices_found):
+    for i in devices_found:
+        for j in range(1, len(devices_found) + 1):
+            window.columnconfigure(0, weight=1, minsize=50)
 
-devices_found = ['No devices found']
+            frame = tk.Frame(master=window, relief=tk.GROOVE, highlightbackground="black", highlightcolor="black", highlightthickness=1 )
+            frame.grid(row=j+2, column=0, padx=3, sticky="ew")
+
+            lbl = tk.Label(master=frame, text=i, bg="white")
+            lbl.pack(fill=tk.X)
 
 
-for i in devices_found:
-    for j in range(1, len(devices_found) + 1):
-        window.columnconfigure(0, weight=1, minsize=50)
+def pingsweep(ip):
+    tkinter.messagebox.showinfo(title=None, message="Scanning your local network...")
+    try:
+        for port in range(1, 1024):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(1)
+            result = s.connect_ex((ip, port))
 
-        frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=2)
-        frame.grid(row=j, column=0, padx=3, pady=3, sticky="ew")
+            if result == 0:
+                devices_found.append(f"Port {port} is open!")
+            s.close()
 
-        lbl = tk.Label(master=frame, text=i, bg="white")
-        lbl.pack(fill=tk.X)
+    except socket.gaierror:
+        print("\n Hostname could not be resolved. Exiting program...")
+        sys.exit()
 
-header.grid(row=1, sticky="ew")
-scan_btn.grid(row=0, column=0, sticky="ew", pady=2)
+    except socket.error:
+        print("\n Couldn't connect to server. Exiting program...")
+        sys.exit()
 
+
+def start_scan(event):
+    # TODO: simple check for now, need to add more stringent IP checking rules
+    if len(router_ip.get()) < 4: 
+        return print('invalid router IP')
+
+    pingsweep(router_ip.get())
+    list_devices(devices_found)
+
+
+scan_btn = tk.Button(master=window, text="Scan your network")
+scan_btn.bind("<Button-1>", start_scan)
+
+header.grid(row=2, sticky="ew")
+scan_btn.grid(row=1, column=0, sticky="ew", pady=2)
+router_ip.grid(row=0, column=0, sticky="ew", pady=1)
 
 window.mainloop()
